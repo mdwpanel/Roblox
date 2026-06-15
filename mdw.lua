866906e005cca16c63d210588b19adc4a6f7c0e5
mdwpanel <mdwpanel@gmail.com>
2026-06-15 18:13:41 +0700

    Update mdw.lua

--- mdw.lua
+++ mdw.lua
@@ -33,7 +33,7 @@ local Config = {
 }
 
 -- Load Modal UI
-local Modal = loadstring(game:HttpGet("https://raw.githubusercontent.com/4LynxX/Libb/refs/heads/main/Lib1.lua"))()
+local Modal = loadstring(game:HttpGet("https://raw.githubusercontent.com/4LynxX/Libb/refs/heads/main/Lib2.lua"))()
 -- ADVANCED ANTI-KICK BYPASS
 local mt = getrawmetatable(game)
 local oldNamecall = mt.__namecall
